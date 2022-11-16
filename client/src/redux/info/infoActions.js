41c81a4e055463307415e26c66564c672e3e7cf2
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-11-15 22:17:29 -0300

    seguridades

--- client/src/redux/info/infoActions.js
+++ client/src/redux/info/infoActions.js
@@ -1,14 +1,22 @@
 import axios from "axios";
+import { allTeams } from "./infoSlice";
 
 export const getAllTeams = () => (dispatch) => {
   axios
-    .get("http://api.cup2022.ir/api/v1/team", {
-      header: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzc0MWU2Y2ZkOWFhYzIyNjcxNzg5ZGIiLCJpYXQiOjE2Njg1NTQ3NTcsImV4cCI6MTY2ODY0MTE1N30.MYcZNkIyZoVNuwbCCKr0glCVQBpfJJGXszRlYLivzRk",
-      },
+    .get("/api/team")
+    .then((resolve) => {
+      //   console.log(resolve.data.data);
+      const teams = resolve.data.data.map((el) => {
+        return {
+          id: el.id,
+          name: el.name_en,
+          fifa_code: el.fifa_code,
+          flag: el.flag,
+          group: el.groups,
+        };
+      });
+      //   console.log(teams);
+      dispatch(allTeams(teams));
     })
-    .then((resolve) => console.log(resolve.data))
     .catch((e) => console.log(e));
 };
