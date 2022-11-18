9deecf663b4f335fd60cc4af3219d667d103f43a
franciscolubo <lupanchox@gmail.com>
2022-11-18 15:24:27 -0300

    styles

--- client/src/Components/Fixture/Fixture.jsx
+++ client/src/Components/Fixture/Fixture.jsx
@@ -20,46 +20,46 @@ export default function Fixture() {
     }
   }, [matchs.length > 0, groupId]);
   return (
-    <div className="lg:pl-[340px] p-8 pt-36">
+    <div className="lg:pl-[340px] md: p-8 pt-36 container mx-auto">
       {match.length > 0 ? (
         <div className="flex flex-col items-center">
           <table>
-            <thead>
-              <tr>
-                <th className="text-white">Fechas</th>
-                <th className="text-white">Horas</th>
-                <th className="text-white">Partidos</th>
-                <th className="text-white align-center">Estadios</th>
+            <thead className="bg-purple-100">
+              <tr >
+                <th className="border border-black">Fechas</th>
+                <th className="border border-black">Horas</th>
+                <th className="border border-black">Partidos</th>
+                <th className="align-center border border-black">Estadios</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className="bg-purple-200">
               {match.map((el) => {
                 return (
                   <tr key={el.id}>
-                    <td className="text-white pr-2 pl-2">
+                    <td className="pr-2 pl-2 font-bold border-x border-black">
                       {el.local_date.slice(0, 5)}
                     </td>
-                    <td className="text-white pr-2 pl-2">
+                    <td className="pr-2 pl-2 font-bold border-x border-black">
                       {setTimeFixture(time, el.local_date)}
                     </td>
-                    <td className="flex justify-between py-2 align-center pr-2 pl-2">
+                    <td className="flex justify-between py-2 align-center pr-2 pl-2 border-r border-black">
                       <div className="flex w-40 items-center">
                         <img
                           className="h-10 w-10"
                           src={el.home_flag}
                           alt="Home flag"
                         />
-                        <p className="text-white text-sm pl-2">
+                        <p className="text-sm pl-2 font-bold">
                           {el.home_team_en.toUpperCase()}
                         </p>
                       </div>
-                      <div className="flex justify-between items-center">
-                        <input className="h-8 w-20" type="number" />
-                        <p className="text-white p-2">-</p>
-                        <input className="h-8 w-20" type="number" />
+                      <div className="flex justify-between items-center font-bold">
+                        <div className="h-8 w-20 bg-white flex items-center"><p className="m-auto">{el.home_score}</p></div>
+                        <p className="p-2 ">-</p>
+                        <div className="h-8 w-20 bg-white flex items-center"><p className="m-auto">{el.away_score}</p></div>
                       </div>
                       <div className="flex w-40 justify-end items-center">
-                        <p className="text-white text-sm pr-2">
+                        <p className="text-sm pr-2 font-bold">
                           {el.away_team_en.toUpperCase()}
                         </p>
                         <img
@@ -69,7 +69,7 @@ export default function Fixture() {
                         />
                       </div>
                     </td>
-                    <td className="text-white pl-3">{stadiums[el.id - 1]}</td>
+                    <td className="pl-3 pr-2 border-r border-black font-bold">{stadiums[el.id - 1]}</td>
                   </tr>
                 );
               })}
